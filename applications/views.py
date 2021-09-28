3eb861d5e39fb46fcd58a65a45a0c840b87ac712
damdream <possible_11@naver.com>
2021-09-28 18:45:41 +0900

    지원서 빈값시 에러 핸들링

--- applications/views.py
+++ applications/views.py
@@ -149,6 +149,9 @@ class ApplicationView(APIView):
             )
             application.recruits.add(recruit)
             
+            if '' == (application.content['career'][0]['leavingDate'] and application.content['career'][0]['joinDate']) and application.content['basicInfo']['phoneNumber']:
+                return JsonResponse ({"message":"DATA_NULL"}, status=400)    
+
             Attachment.objects.create(
                 file_url    = file_url,
                 application = application
